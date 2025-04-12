5e43cc641f6e2bb0fa2c512890bd04bcdba2790e
sukhanjot2033 <sukhanjot9100@gmail.com>
2025-04-12 15:54:45 +0530

    new

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+
+class ClacTests(SimpleTestCase):
+
+    def test_add_numners(self):
+        """Test adding numbers"""
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """Test subtracting numbers"""
+        res = abs(calc.subtract(2,5))
+        self.assertEqual(res, 3)
