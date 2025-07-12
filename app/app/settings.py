99a5452debaac724b8a9742d78a2bc8b885807f4
sukhanjot2033 <sukhanjot9100@gmail.com>
2025-07-12 10:49:08 +0530

    latedt

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,15 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
-# ALLOWED_HOSTS.extend(
-#     filter(
-#         None,
-#         os.environ.get('ALLOWED_HOSTS', '').split(','),
-#     )
-# )
+# DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
